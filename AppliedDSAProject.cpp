fe5b59854334bf45dc630c06c216c4ce4bad542e
Singh <axs230094@utdallas.edu>
2023-11-11 17:56:44 -0600

    moddify karaya hai

--- AppliedDSAProject.cpp
+++ AppliedDSAProject.cpp
@@ -6,50 +6,21 @@
 
 using namespace std;
 
-struct Item
-{
-    string first;
-    string second;
-};
+int main() {
 
-int checkBench(ifstream inputBench) {
-    if(inputBench.fail()) {
+    //Declaring the variables
+    string input, output, filename;
+    ifstream bench;
+
+    //Taking in the input, output and filename
+    cin>>input>>output>>filename;
+
+    //Opening the file
+    bench.open(filename);
+    
+    //Checking if the filename is an actual Benchfile or not
+    if(bench.fail()) {
         cout<<"Wrong file name";
         exit(0);
     }
-    return 0;
-}
-
-int main()
-{
-    string filename, input, output;
-    ifstream file(filename);
-
-    vector<Item> items;
-
-    std::string line;
-    while (std::getline(file, line))
-    {
-        Item item;
-        size_t p_space = line.find_first_of(' ');
-        if(p_space == line.npos){
-            //error: no space between two column
-        }
-        item.first = line.substr(0, p_space);
-        item.second = line.substr(p_space + 1, 
-        line.size() - p_space - 1);
-        items.push_back(item);
-    }
-
-    for (const auto &item : items)
-        std::cout << item.first << ": " << item.second << "\n";
-}
-
-    //Reading Bench File. 
-    ifstream inputBench;
-    inputBench.open(filename);
-    checkBench(inputBench);
-
-    inputBench.close();
-    return 0;
 }
