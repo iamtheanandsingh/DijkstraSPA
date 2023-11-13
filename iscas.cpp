c3358b1c21ab131054f30a06e5479ed6f4e6a786
Singh <axs230094@utdallas.edu>
2023-11-13 00:12:48 -0600

    made improvements

--- iscas.cpp
+++ iscas.cpp
@@ -3,39 +3,75 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <List>
+#include <Map>
 
 using namespace std;
 
-//Declaring BenchFiles Globally
-vector<string> benchFiles = { 
+struct nodeType {
+    const int INPUT;
+    const int OUTPUT;
+    const int INTERMEDIATE;
+};
+
+struct Node {
+    string type, name;
+    list<Node*> adjacencyList;
+    int delay;
+
+    Node(string type, list<Node*> adjacencyList, int delay, string name) {
+        this->type = type;
+        this->adjacencyList = adjacencyList;
+        this->delay = delay;
+        this->name = name;
+    
+    }
+    ~Node() {
+        for (auto nextNode : adjacencyList){
+            delete nextNode;
+        }
+    }
+};
+
+struct pairNode {
+    Node* gate;
+    int delay;
+
+    pairNode(Node* gate, int delay) {
+        this->gate = gate;
+        this->delay = delay;
+    }
+};
+
+list<string> getPath(map<string, string> &penultimate, string destination) {
+    list<string> path;
+    while (!destination.empty()) {
+        path.push_front(destination);
+        destination = penultimate[destination];
+    }
+    return path;
+}
+
+int main() {
+    //Declaring BenchFiles Globally
+    vector<string> benchFiles = { 
         "c17.bench", "c432.bench", "c499.bench", "c880.bench",
         "c1355.bench", "c1908.bench", "c2670.bench", "c3540.bench", 
         "c5315.bench", "c6288.bench", "c7552.bench" 
-        };
-
-int main() {
+    };
+    
     //Declaring the variables
     string input, output, filename;
 
-    cin>>filename>>input>>output;
-    
-    ifstream bench;
-    ifstream inputFile();
-
-    if (!bench.is_open()) {
-        cerr << "Error opening file: " << filePath << endl;
-        return 1;
-    }
-
     //Taking in the input, output and filename
     cin>>filename>>input>>output;
-
-    //Opening the file
-    bench.open(filename);
-    bench
-    //Checking if the filename is an actual Benchfile or not
-    if(bench.fail()) {
+    
+    //be
+    ifstream bench;
+    if(find(benchFiles.begin(), benchFiles.end(), filename) == benchFiles.end()) {
         cout<<"Wrong file name";
         exit(0);
     }
+
+    return 0;
 }
